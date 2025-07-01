46d33a95ee6aca79f358baf3b9fab2b039e5f6d2
minu <min9288@gmail.com>
2025-07-01 22:44:51 +0900

    feat: 상품가격 조회 JPA 쿼리 메서드 추가

--- src/main/java/minu/coffee/productPrice/repositroy/ProductPriceRepo.java
+++ src/main/java/minu/coffee/productPrice/repositroy/ProductPriceRepo.java
@@ -1,7 +1,13 @@
 package minu.coffee.productPrice.repositroy;
 
+import minu.coffee.product.model.Product;
 import minu.coffee.productPrice.model.ProductPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductPriceRepo extends JpaRepository<ProductPrice, Long> {
+
+    Optional<ProductPrice> findByProductAndEnableAndDeleted(Product product, Boolean enable, Boolean deleted);
+
 }
